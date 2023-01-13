f554ef9507cf06139a31cdaeeaefdf1034e74bec
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2023-01-13 14:44:16 +0100

    moved user model to authentication app

--- tracker/issues/models.py
+++ tracker/issues/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    pass
+from tracker.authentication import models as authentication
 
 
 class Comment(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     comment_body = models.TextField("Text")
-    created_by = models.OneToOneField(User, on_delete="DO_NOTHING")
+    created_by = models.OneToOneField(authentication.User, on_delete="DO_NOTHING")
 
 
 # Create your models here.
@@ -23,6 +19,7 @@ class Issue(models.Model):
         (PRIORITY_HIGH, "High"),
         (PRIORITY_LOW, "Low"),
     ]
+
     STATUS_NEW = "New"
     STATUS_ON_HOLD = "On Hold"
     STATUS_WAITING = "Waiting"
@@ -35,6 +32,7 @@ class Issue(models.Model):
         (STATUS_CANCELED, "Canceled"),
         (STATUS_RESOLVED, "Resolved"),
     ]
+
     brief_description = models.TextField("Summary")
     description = models.TextField()
     priority = models.CharField(
@@ -47,10 +45,9 @@ class Issue(models.Model):
     # )
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete="DO_NOTHING")
+    created_by = models.ForeignKey(authentication.User, on_delete="DO_NOTHING")
     # updated_by = models.CharField(max_length=70)
     # assigned_to = models.CharField(max_length=70)
 
     def __str__(self):
         return self.brief_description
-
