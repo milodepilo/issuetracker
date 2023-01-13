26a834de3bf313964ff0e418a3e0516a91e69ead
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2023-01-13 14:20:59 +0100

    added the issue model

--- tracker/issues/models.py
+++ tracker/issues/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Issue(models.Model):
+    PRIORITY_CRIT = "P1"
+    PRIORITY_HIGH = "P2"
+    PRIORITY_LOW = "P3"
+    PRIORITY_CHOICES = [
+        (PRIORITY_CRIT, "Critical"),
+        (PRIORITY_HIGH, "High"),
+        (PRIORITY_LOW, "Low"),
+    ]
+    STATUS_NEW = "New"
+    STATUS_ON_HOLD = "On Hold"
+    STATUS_WAITING = "Waiting"
+    STATUS_CANCELED = "Canceled"
+    STATUS_RESOLVED = "Resolved"
+    STATUS_CHOICES = [
+        (STATUS_NEW, "New"),
+        (STATUS_ON_HOLD, "On Hold"),
+        (STATUS_WAITING, "Waiting"),
+        (STATUS_CANCELED, "Canceled"),
+        (STATUS_RESOLVED, "Resolved"),
+    ]
+    brief_description = models.TextField("Summary")
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=2, choices=PRIORITY_CHOICES, default=PRIORITY_LOW
+    )
+    status = models.TextField(choices=STATUS_CHOICES, default=STATUS_NEW)
+    # added_files = models.FileField(
+    #     "files",
+    #     null=True,
+    # )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    # created_by = models.CharField(max_length=70)
+    # updated_by = models.CharField(max_length=70)
+    # assigned_to = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.brief_description
