daf307cd6035990f80dfbc47ff985a9d55a46b7d
milodepilo <38001005+milodepilo@users.noreply.github.com>
2023-01-17 16:58:24 +0000

    Added url mapping for crud views in issues/url.py

--- tracker/issues/urls.py
+++ tracker/issues/urls.py
@@ -2,9 +2,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name='index'),
-    path('list/', views.IssueListView.as_view(), name='list'),
-    path("open_issue/", views.OpenIssueListView.as_view(), name="open-issues"),
-    path('issue/<int:pk>', views.IssueDetailView.as_view(), name='issue-detail-view'),
-    path('comment/<int:pk>', views.CommentDetailView.as_view(), name='comment-detail-view')
+  path("index/",
+       views.index,
+       name='index'),
+  path('list/',
+       views.IssueListView.as_view(),
+       name='list'),
+  path("open_issue/",
+       views.OpenIssueListView.as_view(), name="open-issues"),
+  path('issue/<int:pk>',
+       views.IssueDetailView.as_view(),
+       name='issue-detail-view'),
+  path('comment/<int:pk>',
+       views.CommentDetailView.as_view(),
+       name='comment-detail-view'),
+  path("issue/<int:pk>/create",
+       views.IssueCreateView.as_view(),
+       name="issue-create-view"),
+  path("issue/<int:pk>/update",
+       views.IssueUpdateView.as_view(),
+       name="issue-update-view"),
+  path("issue/<int:pk>/delete",
+       views.IssueDeleteView.as_view(),
+       name="issue-delete-view")
 ]
